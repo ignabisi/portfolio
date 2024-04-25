f4501eded4f6543ce474c9fc88cb539d6aefbe84
ignacio.bisi <ignacio.bisi@gmail.com>
2024-04-25 18:31:00 +0100

    test new style initial dashboard

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import { ThemeProvider } from '@ui5/webcomponents-react';
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import './styles.css'
+import App from './App'
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-
-root.render(
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+createRoot(document.getElementById('root')).render(<App />)
